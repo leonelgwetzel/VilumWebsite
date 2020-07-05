86c9d2541570abac32b92bba9e0bb27296f17b63
leonelgwetzel <wetzel.leonel.gabriel@gmail.com>
2020-07-05 03:30:11 -0300

    Corrigiendo errores de envío de mail

--- envia.php
+++ envia.php
@@ -1,16 +1,11 @@
 ﻿<?php
 $remitente = $_POST['email'];
-$destinatario = 'leonelgwetzel@outlook.com'; // en esta línea va el mail del destinatario.
+$destinatario = 'ventas@vilum.com.ar'; // en esta línea va el mail del destinatario.
 $asunto = 'CONSULTA WEB-SITE GRUPO VILUM S.R.L.'; // acá se puede modificar el asunto del mail
-if (!$_POST){
-?>
 
-<?php
-}else{
-	 
-    $cuerpo = "Nombre y apellido: " . $_POST["nombre"] . "\r\n"; 
-    $cuerpo .= "Email: " . $_POST["email"] . "\r\n";
-	$cuerpo .= "Consulta: " . $_POST["consulta"] . "\r\n";
+$cuerpo = "Nombre y apellido: " . $_POST["nombre"] . "\r\n"; 
+$cuerpo .= "Email: " . $_POST["email"] . "\r\n";
+$cuerpo .= "Consulta: " . $_POST["consulta"] . "\r\n";
 	//las líneas de arriba definen el contenido del mail. Las palabras que están dentro de $_POST[""] deben coincidir con el "name" de cada campo. 
 	// Si se agrega un campo al formulario, hay que agregarlo acá.
 
@@ -23,6 +18,5 @@ if (!$_POST){
 
     mail($destinatario, $asunto, $cuerpo, $headers);
     
-    include 'confirma.html'; //se debe crear un html que confirma el envío
-}
+    header("Location:index.html#contact");
 ?>
